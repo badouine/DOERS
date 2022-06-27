18853ed9ee4030e0793ed4d7f0bac2ff4641e409
badouine <n>
2022-06-27 19:53:09 +0000

    doer

--- backend/routes/doerRoutes.js
+++ backend/routes/doerRoutes.js
@@ -5,8 +5,8 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "Get doer" });
 });
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get doer" });
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Set doer" });
 });
 router.put("/:id", (req, res) => {
   res.status(200).json({ message: `Update doer ${req.params.id}` });
