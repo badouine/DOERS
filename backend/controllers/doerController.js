c5296e761a0474d95fe1429c35e7035282c26fa9
badouine <n>
2022-06-28 00:56:54 +0000

    async handler

--- backend/controllers/doerController.js
+++ backend/controllers/doerController.js
@@ -1,10 +1,12 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc Get doers
 //@route GET /api/goals -
 //@access Private
 
-const getDoers = (req, res) => {
+const getDoers = asyncHandler(async(req, res) => {
   res.status(200).json({ message: "Get doers" });
-};
+});
 
 
 
@@ -13,30 +15,30 @@ const getDoers = (req, res) => {
 //@access Private
 
 
-const setDoer = (req, res) => {
+const setDoer = asyncHandler(async(req, res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('Please add a text field')
     }
     res.status(200).json({ message: "Set doer" });
-};
+});
 
 // @desc Update doers
 //@route PUT /api/goals/:id
 //@access Private
 
-const updateDoer = (req, res) => {
+const updateDoer = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Update doer ${req.params.id}` });
-}
+})
 
 
 // @desc Delete doers
 //@route PUT /api/goals/:id
 //@access Private
 
-const deleteDoer = (req, res) => {
+const deleteDoer = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Delete doer ${req.params.id}` });
-  }
+  })
 
 module.exports = {
     getDoers,
