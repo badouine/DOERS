74cbf58e592e7d9534783532e0a2ef4d16c65546
badouine <n>
2022-06-28 03:08:23 +0000

    connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
+const colors = require("colors");
 
 const app = express();
-
+connectDB();
 app.use("/api/doers", require("./routes/doerRoutes"));
 app.use(errorHandler);
 
