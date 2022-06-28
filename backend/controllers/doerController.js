4e57919fe7e7eea699ef65be95bf77211b47dfcb
badouine <n>
2022-06-28 03:25:42 +0000

    find doers

--- backend/controllers/doerController.js
+++ backend/controllers/doerController.js
@@ -1,48 +1,47 @@
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require("express-async-handler");
+const Doer = require("../models/doerModel");
 
 // @desc Get doers
 //@route GET /api/goals -
 //@access Private
 
-const getDoers = asyncHandler(async(req, res) => {
-  res.status(200).json({ message: "Get doers" });
+const getDoers = asyncHandler(async (req, res) => {
+  const doers = await Doer.find();
+  res.status(200).json(doers);
 });
-
-
 
 // @desc Set doers
 //@route POST /api/goals -
 //@access Private
 
+const setDoer = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
 
-const setDoer = asyncHandler(async(req, res) => {
-    if(!req.body.text){
-        res.status(400)
-        throw new Error('Please add a text field')
-    }
-    res.status(200).json({ message: "Set doer" });
+  res.status(200).json({ message: "Set doer" });
 });
 
 // @desc Update doers
 //@route PUT /api/goals/:id
 //@access Private
 
-const updateDoer = asyncHandler(async(req, res) => {
-    res.status(200).json({ message: `Update doer ${req.params.id}` });
-})
-
+const updateDoer = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update doer ${req.params.id}` });
+});
 
 // @desc Delete doers
 //@route PUT /api/goals/:id
 //@access Private
 
-const deleteDoer = asyncHandler(async(req, res) => {
-    res.status(200).json({ message: `Delete doer ${req.params.id}` });
-  })
+const deleteDoer = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete doer ${req.params.id}` });
+});
 
 module.exports = {
-    getDoers,
-    setDoer,
-    updateDoer,
-    deleteDoer
-  };
+  getDoers,
+  setDoer,
+  updateDoer,
+  deleteDoer,
+};
