33b6ab1b23221e639902b3f0f2b0ebadb1be9aaf
badouine <n>
2022-06-28 00:44:29 +0000

    middleware

--- backend/controllers/doerController.js
+++ backend/controllers/doerController.js
@@ -14,6 +14,10 @@ const getDoers = (req, res) => {
 
 
 const setDoer = (req, res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
     res.status(200).json({ message: "Set doer" });
 };
 
