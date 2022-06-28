e3039e9efa35bdd2c4a4d57d3587148e104786d8
badouine <n>
2022-06-28 00:46:16 +0000

    middleware

--- backend/server.js
+++ backend/server.js
@@ -6,5 +6,6 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use("/api/doers", require("./routes/doerRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server start on port ${port}`));
