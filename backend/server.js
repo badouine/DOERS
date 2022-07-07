a1a7fa304ba276111ca6ae8f89545a849e7f4bae
badouine <n>
2022-07-07 05:29:56 +0000

    controller

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const app = express();
 connectDB();
 app.use(express.json());
 app.use("/api/doers", require("./routes/doerRoutes"));
+app.use('/api/users', require('./routes/userRoutes'))
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server start on port ${port}`));
